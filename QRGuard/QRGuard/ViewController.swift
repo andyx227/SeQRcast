c2d6024b4f06e26b1dd01daf85a270d7238e0844
Andy <andyxue1@gmail.com>
2019-05-13 13:58:51 -0700

    initial commit

---
+++ QRGuard/QRGuard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRGuard
+//
+//  Created by Andy Xue on 5/13/19.
+//  Copyright © 2019 Ground Zero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
