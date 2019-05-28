cda206f1428c9d995730c6249513d535d55a130a
Andy <andyxue1@gmail.com>
2019-05-27 21:45:13 -0700

    Bug fixed

--- QRGuard/QRGuard/PublishMessageTableViewController.swift
+++ QRGuard/QRGuard/PublishMessageTableViewController.swift
@@ -84,16 +84,20 @@ class PublishMessageTableViewController: UITableViewController {
         
         if contentTextView.text.count > Message.MAX_CONTENT_LENGTH {
             showAlert(withTitle: "Message Publication Error", message: "The message content should not be over \(Message.MAX_CONTENT_LENGTH) characters.")
+            return
         }
-        else if datePicker.date < Date() {
+        else if datePicker.date <= Date() {
             datePicker.minimumDate = Date(timeIntervalSinceNow: 60.0)
             showAlert(withTitle: "Message Publication Error", message: "The expiration date must be after the current time.")
+            return
         }
         else if contentTextView.text.count == 0 {
             showAlert(withTitle: "Message Publication Error", message: "The message content cannot be empty.")
+            return
         }
         else if type == .url, !contentTextView.text.isValidURL {
             showAlert(withTitle: "Message Publication Error", message: "The message content should be in the form of a URL.")
+            return
         }
         
         let message = Message(type: type, expires: datePicker.date, withContent: contentTextView.text, for: channel)
@@ -104,82 +108,10 @@ class PublishMessageTableViewController: UITableViewController {
     
     func showAlert(withTitle title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Confirm", style: .cancel) { (alertAction) in
-            
-        }
+        let action = UIAlertAction(title: "Confirm", style: .cancel)
         alert.addAction(action)
         self.present(alert, animated: true)
     }
-    
-    // MARK: - Table view data source
-
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-    */
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension PublishMessageTableViewController: UITextViewDelegate {
