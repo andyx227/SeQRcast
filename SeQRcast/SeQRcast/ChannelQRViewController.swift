0161578b8aba1c32b7af7acab269c43953be6705
Andy <andyxue1@gmail.com>
2019-05-29 15:06:03 -0700

    Changed app name from 'QRGuard' to 'SeQRcast'

---
+++ SeQRcast/SeQRcast/ChannelQRViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ChannelQRViewController.swift
+//  SeQRcast
+//
+//  Created by Andy Xue on 5/25/19.
+//  Copyright © 2019 Ground Zero. All rights reserved.
+//
+
+import UIKit
+
+class ChannelQRViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var exportImageButton: UIButton!
+    
+    var channelQR: CIImage?
+    
+    var channel = MyChannel()
+    var data: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setShadowForButton(exportImageButton)
+        displayChannelQRCode()
+    }
+    
+    func setShadowForButton(_ button: UIButton) {
+        button.layer.shadowColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0, height: 3)
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowRadius = 10.0
+        button.layer.masksToBounds = false
+    }
+    
+    func displayChannelQRCode() {
+        guard let qr = try? channel.encrypt(with: data), let image = CIContext().createCGImage(qr, from: qr.extent) else {
+            showAlert(withTitle: "QR Code Generation Error", message: "There was an error generating the QR code. Please try again.")
+            return
+        }
+        
+        imageView.image = UIImage(cgImage: image)
+    }
+    
+    func showAlert(withTitle title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Confirm", style: .cancel) { (alertAction) in
+            
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func exportImage(_ sender: UIButton) {
+        if let image = imageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+        else {
+            showAlert(withTitle: "QR Code Export Error", message: "There is no QR code to be exported.")
+        }
+    }
+    
+    @IBAction func returnToMain(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let _ = error {
+            showAlert(withTitle: "QR Code Export Error", message: "There was an error exporting QR code. Please try again.")
+        }
+        showAlert(withTitle: "QR Code Exported", message: "The QR code was successfully exported.")
+    }
+    
+}
+
+
---
+++ SeQRcast/SeQRcast/MessageQRViewController.swift
@@ -0,0 +1,93 @@
+//
+//  MessageQRViewController.swift
+//  SeQRcast
+//
+//  Created by user149673 on 5/26/19.
+//  Copyright © 2019 Ground Zero. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class MessageQRViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var exportImageButton: UIButton!
+    
+    var message = Message()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        exportImageButton.layer.shadowColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        exportImageButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+        exportImageButton.layer.shadowOpacity = 1.0
+        exportImageButton.layer.shadowRadius = 10.0
+        exportImageButton.layer.masksToBounds = false
+        displayChannelQRCode()
+    }
+    
+    func displayChannelQRCode() {
+        guard let qr = try? message.encrypt(), let encoded = try? message.encryptedString(), let image = CIContext().createCGImage(qr, from: qr.extent) else {
+            showAlert(withTitle: "QR Code Generation Error", message: "There was an error generating the QR code. Please try again.")
+            return
+        }
+        
+        let log = MessageLog(for: message, withString: encoded, at: Date())
+        Database.shared.storeLog(log)
+        imageView.image = UIImage(cgImage: image)
+    }
+    
+    func showAlert(withTitle title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Confirm", style: .cancel) { (alertAction) in
+            
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    @IBAction func exportImage(_ sender: UIButton) {
+        if let image = imageView.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+        else {
+            showAlert(withTitle: "QR Code Export Error", message: "There is no QR code to be exported.")
+        }
+    }
+    
+    @IBAction func returnToMain(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let _ = error {
+            showAlert(withTitle: "QR Code Export Error", message: "There was an error exporting QR code. Please try again.")
+        }
+        showAlert(withTitle: "QR Code Exported", message: "The QR code was successfully exported.")
+    }
+    
+}
+
+/*
+ let locationManager = CLLocationManager()
+ locationManager.delegate = self
+ locationManager.requestLocation()
+extension MessageQRViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first, let encoded = try? message.encryptedString() else {
+            return
+        }
+        let log = MessageLog(for: message, withLatitude: location.coordinate.latitude, andLongitude: location.coordinate.longitude, withString: encoded, at: Date())
+        Database.shared.storeLog(log)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard let encoded = try? message.encryptedString() else {
+            return
+        }
+        let log = MessageLog(for: message, withString: encoded, at: Date())
+        Database.shared.storeLog(log)
+    }
+}
+*/
